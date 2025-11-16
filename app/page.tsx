54ab500f230cbb68f0586437ad8db753c8f64b8f
Dev-Ramos <lucaxiDev@gmail.com>
2025-11-16 00:23:04 -0300

    feat: add booking item component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Header from "./_components/Header";
 import SearchInput from "./_components/SearchInput";
 import Image from "next/image";
 import banner from "@/public/banner.png";
+import BookingItem from "./_components/BookingItem";
 
 export default function Home() {
   return (
@@ -14,6 +15,13 @@ export default function Home() {
           alt="Agende agora!"
           sizes="100vw"
           className="h-auto w-full"
+        />
+        <h2 className="text-xs text-foreground font-semibold uppercase">Agendamentos</h2>
+        <BookingItem
+          barbershopName="Barbearia Vintage"
+          serviceName="Corte de Cabelo"
+          date={new Date()}
+          barbershopImageUrl="https://utfs.io/f/5832df58-cfd7-4b3f-b102-42b7e150ced2-16r.png"
         />
       </div>
     </main>
